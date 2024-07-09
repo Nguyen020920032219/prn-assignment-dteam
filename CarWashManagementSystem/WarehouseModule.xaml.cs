6a238294e1fcdd9a2774d97b06f640b949345b34
Trần Đình Nguyên <trandinhnguyen46159@gmail.com>
2024-07-09 20:53:10 +0700

    add logic for warehouse

---
+++ CarWashManagementSystem/WarehouseModule.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Repository.Entities;
+using Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CarWashManagementSystem
+{
+    /// <summary>
+    /// Interaction logic for ServiceModule.xaml
+    /// </summary>
+    public partial class WarehouseModule : Window
+    {
+        ProductService _productService;
+        public WarehouseModule()
+        {
+            InitializeComponent();
+            _productService = new ProductService();
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            var Name = txtName.Text;
+            var Description=txtDescription.Text;
+            var Price= decimal.Parse(txtPrice.Text);
+            var Quantity= Int32.Parse(txtQuantity.Text);
+
+            Product product = new Product();
+            product.Name = Name;
+            product.Description = Description;
+            product.Price = Price;
+            product.StockQuantity = Quantity;
+            product.IsDeleted = false;
+
+            _productService.CreateProduct(product);
+
+            Close_Click(sender, e);
+        }
+    }
+}
